e95558556780393b37ea738bc1db67504d94a10a
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2019-08-19 16:56:28 -0300

    First Version.

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM debian:10 as builder
+RUN apt-get update && \
+    apt-get install autoconf automake autotools-dev g++ pkg-config python-dev libtool make git curl ca-certificates -y
+
+ENV DUMB_INIT_VERSION=1.2.0
+RUN curl -L -o /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v${DUMB_INIT_VERSION}/dumb-init_${DUMB_INIT_VERSION}_amd64 && \
+    chmod +x /usr/local/bin/dumb-init
+
+WORKDIR /usr/src
+
+RUN git clone https://github.com/uber/pyflame.git . && \
+    ./autogen.sh && \
+    ./configure && \
+    make install
+
+RUN curl -o /usr/local/bin/flame-chart-json https://raw.githubusercontent.com/uber/pyflame/master/utils/flame-chart-json && \
+    chmod +x /usr/local/bin/flame-chart-json
+
+
+FROM python:2.7.16-slim as final
+
+COPY --from=builder /usr/local/bin/pyflame /usr/local/bin/pyflame
+COPY --from=builder /usr/local/bin/flame-chart-json /usr/local/bin/flame-chart-json
+COPY --from=builder /usr/local/bin/dumb-init /usr/local/bin/dumb-init
+
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y gcc libc6-dev && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /usr/src
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+COPY gunicorn_cpu_monitor.py .
+
+ENTRYPOINT ["/usr/local/bin/dumb-init","python","gunicorn_cpu_monitor.py"]
---
+++ gunicorn_cpu_monitor.py
@@ -0,0 +1,64 @@
+import logging
+import sys
+import threading
+
+import plumbum
+import psutil
+import os
+
+from time import sleep
+
+
+pyflame = plumbum.local["pyflame"]
+flamechartjson = plumbum.local["flame-chart-json"]
+
+FLAMECHARTS_FOLDER = os.getenv('FLAMECHARTS_FOLDER')
+CPU_THRESHOLD = os.getenv('CPU_THRESHOLD', 90)
+SCAN_INTERVAL = os.getenv('SCAN_INTERVAL', 60)
+GUNICORN_PARENT_PID = os.getenv('GUNICORN_PARENT_PID', 1)
+
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.level = logging.INFO
+
+    return logger
+
+
+def get_gunicorn_high_cpu_children_processes(gunicorn_master_pid, threshold=CPU_THRESHOLD):
+    gunicorn_subprocesses = psutil.Process(gunicorn_master_pid).children(recursive=True)
+
+    return [children for children in gunicorn_subprocesses if children.cpu_percent() > threshold]
+
+
+def generate_flamechart_files_for_processes(processes):
+    for process in processes:
+        cpu_profile = "{}/process_{}.profile".format(FLAMECHARTS_FOLDER, process.pid)
+        (pyflame(["-s", "5", "-p", process.pid, "--threads", "--flamechart"]) > flamechartjson >
+         cpu_profile)()
+
+
+def start_cpu_monitor_thread(gunicorn_master_pid, sleep_time=SCAN_INTERVAL):
+    logger = setup_logging()
+    while True:
+        gunicorn_high_cpu_children_processes = get_gunicorn_high_cpu_children_processes(gunicorn_master_pid)
+
+        logger.info("Starting Monitoring thread")
+        logger.info("{} processes will be profiled.".format(len(gunicorn_high_cpu_children_processes)))
+
+        monitor_thread = threading.Thread(target=generate_flamechart_files_for_processes,
+                                          args=(gunicorn_high_cpu_children_processes,))
+        monitor_thread.daemon = True
+        monitor_thread.start()
+        logger.info("Monitoring thread for this cycle is done. Sleeping.\n")
+
+        sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    gunicorn = psutil.Process(GUNICORN_PARENT_PID).children()
+    if not gunicorn:
+        raise ValueError("Gunicorn processes not found. Check the `GUNICORN_PARENT_PID` env var documentation.")
+
+    start_cpu_monitor_thread(gunicorn[0].pid)
