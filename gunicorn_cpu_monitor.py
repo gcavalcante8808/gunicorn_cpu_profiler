9e90e9e6a2b7e89e76de7fb674881288230e5156
Gabriel Abdalla Cavalcante <gabriel.cavalcante88@gmail.com>
2019-08-20 16:09:54 -0300

    Update gunicorn_cpu_monitor.py

--- gunicorn_cpu_monitor.py
+++ gunicorn_cpu_monitor.py
@@ -14,6 +14,7 @@ flamechartjson = plumbum.local["flame-chart-json"]
 
 FLAMECHARTS_FOLDER = os.getenv('FLAMECHARTS_FOLDER')
 CPU_THRESHOLD = int(os.getenv('CPU_THRESHOLD', 90))
+CPU_READ_INTERVAL= int(os.getenv('CPU_READ_INTERVAL', 5)
 SCAN_INTERVAL = int(os.getenv('SCAN_INTERVAL', 60))
 GUNICORN_PARENT_PID = int(os.getenv('GUNICORN_PARENT_PID', 1))
 
@@ -29,7 +30,7 @@ def setup_logging():
 def get_gunicorn_high_cpu_children_processes(gunicorn_master_pid, threshold=CPU_THRESHOLD):
     gunicorn_subprocesses = psutil.Process(gunicorn_master_pid).children(recursive=True)
 
-    return [children for children in gunicorn_subprocesses if children.cpu_percent() > threshold]
+    return [children for children in gunicorn_subprocesses if children.cpu_percent(interval=CPU_READ_INTERVAL) > threshold]
 
 
 def generate_flamechart_files_for_processes(processes):
